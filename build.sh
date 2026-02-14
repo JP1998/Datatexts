cf94157e4f93cfea9737567a831b2a0be8c2937c
JP1998 <littlebigjonny06@gmail.com>
2026-02-14 03:49:07 +0100

    Modify build script to have a modifiable original addon name

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ set -o allexport && source ./build-settings.conf && set +o allexport
 rsync -av --exclude-from=".buildignore" --delete-excluded . "$ADDON_FOLDER/$ADDON_RENAME"
 
 # Rename the .toc file to reflect the rename of the addon folder
-mv "$ADDON_FOLDER/$ADDON_RENAME/GratzieWitch.toc" "$ADDON_FOLDER/$ADDON_RENAME/$ADDON_RENAME.toc"
+mv "$ADDON_FOLDER/$ADDON_RENAME/$ADDON_NAME.toc" "$ADDON_FOLDER/$ADDON_RENAME/$ADDON_RENAME.toc"
 
 # Append a "(Development)" flag to the addon name in the toc-file to be able to identify it in-game
 sed -i -r -e 's/^## Title: (.*)$/## Title: \1 (Development)/g' "$ADDON_FOLDER/$ADDON_RENAME/$ADDON_RENAME.toc"
