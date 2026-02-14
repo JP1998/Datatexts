68e0e521d12a4f7376946117ed00e7bd9972ee30
JP1998 <littlebigjonny06@gmail.com>
2026-02-14 12:03:43 +0100

    Refactor code styling

--- modules/time.lua
+++ modules/time.lua
@@ -10,7 +10,6 @@ app.Time.Initialize = function()
     app.Time.Frame:SetFont("Fonts/FRIZQT__.TTF", 28, "THICKOUTLINE");
     app.Time.Frame:SetTextColor(1, 1, 1);
     app.Time.Frame:SetShadowColor(0, 0, 0);
-    -- app.Time.Frame:SetText("10|cFF0070DD:|r29 |cFF0070DDPM|r");
     app.Time.Frame:Show();
 
     app.Time.ampm = "";
@@ -21,40 +20,43 @@ end
 app.Time.OnUpdate = function()
     local c = app.Settings:Get("Time");
 
-    app.Time.minute = date("%M")
+    app.Time.minute = date("%M");
+
     if c.localTime then
         if c.twentyFourHour then
-            app.Time.hour = date("%H")
+            app.Time.hour = date("%H");
         else
-            app.Time.ampm = date("%p")
-            app.Time.hour = date("%I")
-            app.Time.hour = app.Time.hour:match("0*(%d+)")
+            app.Time.ampm = date("%p");
+            app.Time.hour = date("%I");
+            app.Time.hour = app.Time.hour:match("0*(%d+)");
         end
     else
-        app.Time.hour = GetGameTime()
+        app.Time.hour = GetGameTime();
+
         if c.twentyFourHour then
-            app.Time.hour = "0"..tostring(app.Time.hour)
+            app.Time.hour = "0"..tostring(app.Time.hour);
         else
             if app.Time.hour > 12 then
-                app.Time.hour = math.abs(app.Time.hour - 12)
-                app.Time.ampm = "PM"
+                app.Time.hour = math.abs(app.Time.hour - 12);
+                app.Time.ampm = "PM";
             elseif app.Time.hour == 0 then
-                app.Time.hour = 12
-                app.Time.ampm = "AM"
+                app.Time.hour = 12;
+                app.Time.ampm = "AM";
             else
-                app.Time.hour = app.Time.hour
-                app.Time.ampm = "AM"
+                app.Time.hour = app.Time.hour;
+                app.Time.ampm = "AM";
             end
-            app.Time.hour = tostring(app.Time.hour)
-            app.Time.hour = app.Time.hour:match("0*(%d+)")
+
+            app.Time.hour = tostring(app.Time.hour);
+            app.Time.hour = app.Time.hour:match("0*(%d+)");
         end
     end
 
     local updatedTime = "";
     if c.twentyFourHour then
-        updatedTime = ("%s%s%s"):format(app.Time.hour, app.Time.colorString(":"), app.Time.minute);
+        updatedTime = ("%s%s%s"):format(app.Time.hour, app.colorString(":"), app.Time.minute);
     else
-        updatedTime = ("%s%s%s%s"):format(app.Time.hour, app.Time.colorString(":"), app.Time.minute, app.Time.colorString(app.Time.ampm));
+        updatedTime = ("%s%s%s %s"):format(app.Time.hour, app.colorString(":"), app.Time.minute, app.colorString(app.Time.ampm));
     end
 
     app.Time.Frame:SetText(updatedTime);
